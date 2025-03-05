706d1fb20bc6ab4a329751feb5ba6381a5ffab4c
Nemya <yiduolnemya@gmail.com>
2025-03-04 21:51:11 -0500

    feat: add signin button with clerk

--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,14 @@
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <SignInButton mode="modal" forceRedirectUrl="/">
+        <Button>Sign in</Button>
+      </SignInButton>
+    </div>
+  );
 }
 
 export default Home;
