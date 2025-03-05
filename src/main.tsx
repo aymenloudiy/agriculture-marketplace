5eef21c873b4f8a988edb6029a9af1d9b9167a84
Nemya <yiduolnemya@gmail.com>
2025-03-04 21:50:38 -0500

    feat: add clerk auth provider component

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,21 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./home";
 import Contact from "./contact";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/contact", element: <Contact /> },
 ]);
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>
 );
