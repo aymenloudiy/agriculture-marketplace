1a9cd8bc2d2286c0649497a3e73f8e69ad88cd28
Nimuel <yiduolnemya@gmail.com>
2025-03-21 00:03:55 -0400

    Add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { styles } from "./navbar.module.css";
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <div>Logo</div>
+        <ul>
+          <li>Shop</li>
+          <li>About</li>
+          <li>Cart</li>
+          <li>Sign in</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
