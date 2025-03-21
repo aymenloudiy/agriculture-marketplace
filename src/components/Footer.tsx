7d11ed9901dae92047f8032afbd76bef737b5dd3
Nimuel <yiduolnemya@gmail.com>
2025-03-21 00:04:48 -0400

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { styles } from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>Logo</div>
+      <div>
+        <div>
+          <div>About</div>
+          <ul>
+            <li>Agrilink</li>
+            <li>AgriTeam</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>Follow Us</div>
+          <ul>
+            <li>Instagram</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>Legal</div>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Terms & Conditions</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
