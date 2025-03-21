462f00058f6ab1c9dfd376630ea82b8e9ded5b39
Nimuel <yiduolnemya@gmail.com>
2025-03-21 00:04:58 -0400

    Add Categories component

---
+++ src/components/Categories.tsx
@@ -0,0 +1,16 @@
+import { styles } from "./categories.module.css";
+
+export default function categories() {
+  return (
+    <div>
+      <ul>
+        <li>Honey</li>
+        <li>Oil</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+      </ul>
+    </div>
+  );
+}
