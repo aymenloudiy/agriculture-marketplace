288314f78667ca4edf04cddd6d7134a0a44c47c4
Nimuel <yiduolnemya@gmail.com>
2025-03-21 11:15:16 -0400

    feat: set the elements within the main page feat: add mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/data/data.tsx
@@ -0,0 +1,31 @@
+const items = {
+  item1: {
+    id: crypto.randomUUID(),
+    product_name: "Miel_1",
+    product_price: "10",
+    product_currency: "MAD",
+    product_image: "./assets/",
+  },
+  item2: {
+    id: crypto.randomUUID(),
+    product_name: "Miel_2",
+    product_price: "10",
+    product_currency: "MAD",
+    product_image: "./assets/",
+  },
+  item3: {
+    id: crypto.randomUUID(),
+    product_name: "huile_1",
+    product_price: "10",
+    product_currency: "MAD",
+    product_image: "./assets/",
+  },
+  item4: {
+    id: crypto.randomUUID(),
+    product_name: "huile_2",
+    product_price: "10",
+    product_currency: "MAD",
+    product_image: "./assets/",
+  },
+};
+export default items;
