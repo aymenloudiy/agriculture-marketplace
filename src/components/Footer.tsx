374efc01e3aa315ddfbab4c70d2c5c633acdb79c
Nimuel <yiduolnemya@gmail.com>
2025-03-28 14:27:49 -0400

    feat: re-adjust footer to reflect design changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,33 +3,56 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>Logo</div>
       <div>
         <div>
-          <div>About</div>
+          <p>Got a question? Contact us</p>
+        </div>
+        <div>
+          <h3>Company</h3>
           <ul>
-            <li>Agrilink</li>
-            <li>AgriTeam</li>
+            {/* TODO: All these should be NavLinks */}
+            <li>
+              <a href="">About company</a>
+            </li>
+            <li>
+              <a href="">Our team</a>
+            </li>
+            <li>
+              <a href="">Careers</a>
+            </li>
+            <li>
+              <a href="">Contact us</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Account</h3>
+          <ul>
+            <li>Your account</li>
+            <li>Shipping rates and policies</li>
+            <li>Refunds and replacements</li>
+            <li>Delivery info</li>
+            <li>Order tracking</li>
+            <li>Taxes and fees</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Customer Service</h3>
+          <ul>
+            <li>Payment methods</li>
+            <li>Money back guarantee</li>
+            <li>Product returns</li>
+            <li>Support center</li>
+            <li>Shipping</li>
+            <li>Terms and conditions</li>
           </ul>
         </div>
       </div>
       <div>
-        <div>
-          <div>Follow Us</div>
-          <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-          </ul>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>Legal</div>
-          <ul>
-            <li>Privacy Policy</li>
-            <li>Terms & Conditions</li>
-          </ul>
-        </div>
+        <small>&copy; All rights reserved. Made by Agrilinks</small>
       </div>
     </footer>
   );
