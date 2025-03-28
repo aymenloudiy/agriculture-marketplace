b232bb396b7e206ce0e8a3f60abfbddd523710f3
Nimuel <yiduolnemya@gmail.com>
2025-03-28 14:45:58 -0400

    feat: adjust item card to reflect design changes feat: add a hero component

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,5 +1,26 @@
 import styles from "./itemCard.module.css";
 
 export default function ItemCard() {
-  return <div className={styles.itemCard}>ItemCard</div>;
+  return (
+    <div className={styles.itemCard}>
+      <div>
+        <p>discount tag</p>
+        <img src="" alt="Product-name" />
+      </div>
+      <div>
+        <div>Rating</div>
+        <div>Number of reviews</div>
+      </div>
+      <div>
+        <p>Product Name</p>
+      </div>
+      <div>
+        <div>
+          <p>New price</p>
+          <p>Old price</p>
+        </div>
+        <div>Cart icon</div>
+      </div>
+    </div>
+  );
 }
