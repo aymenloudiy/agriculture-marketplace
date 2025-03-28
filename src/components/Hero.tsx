c76ac93f2ba9929b8b8d5957d6258e042f59b31f
Nimuel <yiduolnemya@gmail.com>
2025-03-28 14:49:57 -0400

    feat: add newsLetter component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
 export default function Hero() {
+  // TODO: This needs to be a carousel
   return <div>Hero</div>;
 }
---
+++ src/components/NewsLetter.tsx
@@ -0,0 +1,18 @@
+export default function NewsLetter() {
+  return (
+    <div>
+      <h2>Sign up to our newsletter</h2>
+      <p>Receive our latest updates about our products and promotions</p>
+      <form action="">
+        <input type="text" name="email" id="email" />
+        <label htmlFor="email">Subscribe</label>
+      </form>
+      <ul>
+        <li>Instagram icon</li>
+        <li>Facebook icon</li>
+        <li>Youtube icon</li>
+        <li>Telegram icon</li>
+      </ul>
+    </div>
+  );
+}
