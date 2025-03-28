77365c18dfa2807ca9d378b9524ef73144e5a957
Nimuel <yiduolnemya@gmail.com>
2025-03-28 15:20:42 -0400

    feat: add product container component

---
+++ src/components/Product/ProductContainer.tsx
@@ -0,0 +1,29 @@
+import ProductCarousel from "./ProductCarousel";
+import ProductDetails from "./ProductDetails";
+import ProductShippingInfo from "./ProductShippingInfo";
+
+export default function ProductContainer() {
+  return (
+    <div>
+      <h2>Product Name</h2>
+      <nav>
+        {/* TODO: These should be NavLinks */}
+        <ul>
+          <li>
+            <a href="">General Info</a>
+          </li>
+          <li>
+            <a href="">Product Details</a>
+          </li>
+          <li>
+            <a href="">Reviews</a>
+          </li>
+        </ul>
+      </nav>
+      {/* TODO: This needs to be an outlet, change based on above NavLink */}
+      <ProductCarousel />
+      <ProductShippingInfo />
+      <ProductDetails />
+    </div>
+  );
+}
