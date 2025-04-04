81b3e932932259d0002a50ddbfebb632e37840c2
Nimuel <yiduolnemya@gmail.com>
2025-04-04 15:25:53 -0400

    fix: wrap list item content in an anchor tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>
-        <div>
+      <div className={styles.footer_content}>
+        <div className={styles.footer_content_contact}>
           <p>Got a question? Contact us</p>
         </div>
-        <div>
+        <div className={styles.footer_content_list}>
           <h3>Company</h3>
           <ul>
             {/* TODO: All these should be NavLinks */}
@@ -28,30 +28,54 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <div>
+        <div className={styles.footer_content_list}>
           <h3>Account</h3>
           <ul>
-            <li>Your account</li>
-            <li>Shipping rates and policies</li>
-            <li>Refunds and replacements</li>
-            <li>Delivery info</li>
-            <li>Order tracking</li>
-            <li>Taxes and fees</li>
+            <li>
+              <a href="">Your account</a>
+            </li>
+            <li>
+              <a href="">Shipping rates and policies</a>
+            </li>
+            <li>
+              <a href="">Refunds and replacements</a>
+            </li>
+            <li>
+              <a href="">Delivery info</a>
+            </li>
+            <li>
+              <a href="">Order tracking</a>
+            </li>
+            <li>
+              <a href="">Taxes and fees</a>
+            </li>
           </ul>
         </div>
-        <div>
+        <div className={styles.footer_content_list}>
           <h3>Customer Service</h3>
           <ul>
-            <li>Payment methods</li>
-            <li>Money back guarantee</li>
-            <li>Product returns</li>
-            <li>Support center</li>
-            <li>Shipping</li>
-            <li>Terms and conditions</li>
+            <li>
+              <a href="">Payment methods</a>
+            </li>
+            <li>
+              <a href="">Money back guarantee</a>
+            </li>
+            <li>
+              <a href="">Product returns</a>
+            </li>
+            <li>
+              <a href="">Support center</a>
+            </li>
+            <li>
+              <a href="">Shipping</a>
+            </li>
+            <li>
+              <a href="">Terms and conditions</a>
+            </li>
           </ul>
         </div>
       </div>
-      <div>
+      <div className={styles.footer_rights}>
         <small>&copy; All rights reserved. Made by Agrilinks</small>
       </div>
     </footer>
